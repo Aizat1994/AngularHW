d4f36aa190b88353a0172d0c0f84320884435642
garifullin aizat <garifullinaizat@gmail.com>
2022-10-15 18:42:56 +0600

    Hw

---
+++ Angular HW/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PostListComponent } from './post-list/post-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserListComponent,
+    UserDetailsComponent,
+    PostListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
